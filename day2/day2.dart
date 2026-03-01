1a1f37c41b8d853c03067a1789970023c3fe6383
Lekha Pantha <lekhapantha14@gmail.com>
2026-03-02 00:28:21 +0545

    Day 01 & Day 02: Initial commit with Dart basics

---
+++ day2/day2.dart
@@ -0,0 +1,15 @@
+void main() {
+  int age = 21;
+  double height = 5.8;
+  String name = "Lekha";
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Student : $isStudent");
+
+  print(
+    "Hello! My name is ${name}. I am ${age}, my height is ${height}, and student status is ${isStudent}.",
+  );
+}
